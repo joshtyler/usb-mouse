ac86e143f027e2336cdff4192878fc989ffcddfd
joshtyler <josh.tyler@btinternet.com>
2016-10-11 21:24:18 +0100

    Initial commit. Empty Keil project

---
+++ src/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'usb-mouse' 
+ * Target:  'dev_board' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "MKL46Z4.h"
+
+
+#endif /* RTE_COMPONENTS_H */
